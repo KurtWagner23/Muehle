35f88decc12f2d6e942d3afb1df802ddc76c515e
KurtWagner23 <philip.boehringer@web.de>
2025-02-05 20:27:12 +0100

    First gui test

--- gui/main.cpp
+++ gui/main.cpp
@@ -2,14 +2,43 @@
 #include "raylib.h"
 
 int main() {
-    InitWindow(800, 600, "Mühle GUI mit Raylib");
+    const int screenWidth = 800;
+    const int screenHeight = 1000;
 
+    InitWindow(screenWidth, screenHeight, "Muehle Game");
+
+    Texture2D boardTexture = LoadTexture("assets/muehle.png");
+    Vector2 piecePos = {400.0f, 300.0f};
+
+    SetTargetFPS(60);
+
+    std::vector<Vector2> positions = {};
+
+    ClearBackground(RAYWHITE);
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Hallo, Mühle GUI!", 250, 280, 20, DARKGRAY);
+
+        DrawTexturePro(
+            boardTexture,
+            {0, 0, (float)boardTexture.width, (float)boardTexture.height},
+            {0, 0, (float)screenWidth, (float)screenHeight - 200.0},
+            {0, -200},
+            0.0f,
+            WHITE);
+
+        Vector2 mousePosition = GetMousePosition();
+        piecePos = mousePosition;
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            positions.push_back(piecePos);
+        }
+        for (auto pos : positions) {
+            DrawCircleV(pos, 20.0f, RED);
+        }
+
         EndDrawing();
     }
+
+    UnloadTexture(boardTexture);
 
     CloseWindow();
     return 0;
